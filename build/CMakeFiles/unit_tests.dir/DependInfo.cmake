a88be5729128f3fba17bf41334d98e5c238ec236
Mushagi MAYIBO <mmayibo@e4r23p3.wethinkcode.co.za>
2018-01-09 14:55:50 +0200

    last adding

--- build/CMakeFiles/unit_tests.dir/DependInfo.cmake
+++ build/CMakeFiles/unit_tests.dir/DependInfo.cmake
@@ -4,9 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/vador/projects/expertsystem/src/test/unit_tests.cpp" "/home/vador/projects/expertsystem/build/CMakeFiles/unit_tests.dir/src/test/unit_tests.cpp.o"
+  "/goinfre/mmayibo/projects/expertsystem/src/test/unit_tests.cpp" "/goinfre/mmayibo/projects/expertsystem/build/CMakeFiles/unit_tests.dir/src/test/unit_tests.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
@@ -15,7 +15,7 @@ set(CMAKE_CXX_TARGET_INCLUDE_PATH
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
-  "/home/vador/projects/expertsystem/build/CMakeFiles/expert_system_classes.dir/DependInfo.cmake"
+  "/goinfre/mmayibo/projects/expertsystem/build/CMakeFiles/expert_system_classes.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
